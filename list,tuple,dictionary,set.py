537f624dfcf932cfba43ebda016548a0e9c028e9
j.z.fowler <j.z.fowler@student.saintkentigern.com>
2023-02-26 10:43:02 +1300

    How to access a list using negative indexing

--- list,tuple,dictionary,set.py
+++ list,tuple,dictionary,set.py
@@ -1,4 +1,4 @@
-print("Introducing lists\n")
+'''HOW TO MAKE A BASIC LIST'''
 
 #Making a python list with 5 integers
 numbers = [1, 2, 3, 4, 5]
@@ -6,8 +6,9 @@ numbers = [1, 2, 3, 4, 5]
 #printing all numbers in the list
 print(numbers)
 
-#How to access elements within a list
-#Note that indexing starts at zero
+
+'''#HOW TO ACCESS ELEMENTS IN A LIST, 
+NOTE INDEXING STARTS FROM 0'''
 
 #New list of computer games
 computer_games = ["csgo", "valorant", "forza"]
@@ -19,3 +20,11 @@ print(computer_games[0])
 print(computer_games[2])
 
 
+'''NEGATIVE INDEXING'''
+
+#New csgo gun list
+types_of_csgo_guns = ["p250", "AWP", "m4a4", "glock-18", "negev" ]
+
+#how to acess an item on a list with negative indexing
+print(types_of_csgo_guns[-3])
+print(types_of_csgo_guns[-5])

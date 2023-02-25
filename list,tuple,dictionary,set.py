22d9ef58e6a648a627eb51feb456ba5fd4398a05
j.z.fowler <j.z.fowler@student.saintkentigern.com>
2023-02-26 10:31:30 +1300

    How to access a list

--- list,tuple,dictionary,set.py
+++ list,tuple,dictionary,set.py
@@ -1,10 +1,21 @@
 print("Introducing lists\n")
 
 #Making a python list with 5 integers
-
-numbers = [1,2,3,4,5]
+numbers = [1, 2, 3, 4, 5]
 
 #printing all numbers in the list
-
 print(numbers)
+
+#How to access elements within a list
+#Note that indexing starts at zero
+
+#New list of computer games
+computer_games = ["csgo", "valorant", "forza"]
+
+#how to access "csgo" on the list
+print(computer_games[0])
+
+#How to access "forza" on the lost
+print(computer_games[2])
+
 

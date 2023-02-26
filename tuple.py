6212be0b327e54c515e3f015cc56f2ebd83f76a1
j.z.fowler <j.z.fowler@student.saintkentigern.com>
2023-02-26 20:55:13 +1300

    How to make tuple & Accesing elements using indexing

---
+++ tuple.py
@@ -0,0 +1,28 @@
+'''HOW TO MAKE A TUPLE'''
+
+#Empty tuple
+empty_tuple = ()
+print(empty_tuple)
+
+#Tuple with integers
+integer_tuple = (1, 2, 3, 4, 5)
+print(integer_tuple)
+
+#tuple with mixed datatypes
+mixed_tuple = (1, 2, "hello", 3, "goodbye", 4)
+print(mixed_tuple)
+
+'''ACCESSING TUPLE ELEMENTS USING INDEXING'''
+alphabet = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",)
+print(alphabet[5])
+print(alphabet[3])
+print(alphabet[0])
+print(alphabet[7])
+
+
+'''ACCESSING TUPLE ELEMENTS USING NEGATIVE INDEXING'''
+alphabet = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",)
+print(alphabet[-2])
+print(alphabet[-6])
+print(alphabet[-3])
+print(alphabet[-7])

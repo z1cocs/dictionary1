e944f091e7ce07e1e71f590abae7aa015be183f5
j.z.fowler <j.z.fowler@student.saintkentigern.com>
2023-02-26 21:28:50 +1300

    Creating dictionary + Accessing Elements

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+'''CREATING A DICTIONARY'''
+
+players_from_each_sport = {"Football": "Messi", "Basketball": "LeBron James", "Golf": "Tiger Woods"}
+print(players_from_each_sport)
+
+
+'''ACCESSING ELEMENTS FROM DICTIONARY'''
+
+players_from_each_sport = {"Football": "Messi", "Basketball": "LeBron James", "Golf": "Tiger Woods"}
+
+print(players_from_each_sport["Football"])
+print(players_from_each_sport["Golf"])

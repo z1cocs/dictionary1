29d853232f5a4cbca6121024d35341e0faace9b6
j.z.fowler <j.z.fowler@student.saintkentigern.com>
2023-02-26 21:35:56 +1300

    Finding item in a dicitonary based of user input

--- dictionary.py
+++ dictionary.py
@@ -10,3 +10,14 @@ players_from_each_sport = {"Football": "Messi", "Basketball": "LeBron James", "G
 
 print(players_from_each_sport["Football"])
 print(players_from_each_sport["Golf"])
+
+
+'''FINDING ITEMS IN A DICTIONARY BASED OF USER INPUT'''
+
+players_from_each_sport = {"Football": "Messi", "Basketball": "LeBron James", "Golf": "Tiger Woods"}
+search = input("What sport do you want me to find a player from:")
+
+if search in players_from_each_sport:
+    print("Aha, a player from the sport", search, "is", players_from_each_sport[search])
+else:
+    print("Your chosen sport is not in my dictionary")

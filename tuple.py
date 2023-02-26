f9d7a2f4e9a30b74f07476b3081e6885c59a40ca
j.z.fowler <j.z.fowler@student.saintkentigern.com>
2023-02-26 21:08:34 +1300

    How to find a item in tuple based of user input

--- tuple.py
+++ tuple.py
@@ -26,3 +26,22 @@ print(alphabet[-2])
 print(alphabet[-6])
 print(alphabet[-3])
 print(alphabet[-7])
+
+
+'''HOW TO PRINT EVERY ITEM OF A TUPLE ON DIFFERENT LINES'''
+alphabet = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",)
+
+for item in alphabet:
+    print(item)
+
+
+'''HOW TO FIND A ITEM IN A TUPLE BASED OFF INPUT FROM A USER'''
+
+#New multiple tuple element
+csgo_t_guns = ("ak47", "glock", "mac-10", "ssg 553", "tec-9")
+
+search_for_item = input("Enter a terrorist weapon:")
+if search_for_item in csgo_t_guns:
+    print("Yep, found it!")
+else:
+    print("terrorist side weapon not found!")
